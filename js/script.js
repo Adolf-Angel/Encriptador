011505dc5d87229db8bd035d1004515187681894
Adolf-Angel <adotolentinosan90@gmail.com>
2023-05-24 19:11:47 -0600

    iniciando con el encriptador

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+const encriptar = document.querySelector(".btn-encriptar")
+encriptar.addEventListener('click', (e) =>{
+    const texto = document.querySelector("textin");
+   // e.preventdefault();
+
+    console.log("encriptando");
+
+})
