5bc429c01fd2136339a297c475f24b505e60b655
Adolf-Angel <adotolentinosan90@gmail.com>
2023-05-30 00:31:45 -0600

    se agrego funcionalidad desencriptar y esta todo listo

--- js/script.js
+++ js/script.js
@@ -1,25 +1,54 @@
 
-function encriptar(){
+function encriptar() {
     let tex = document.getElementById("texto").value;
-    // let Mensaje = document.getElementById("mensaje-principal");
-    // let parrafo = document.getElementById("parrafo-in");
-    // let imagen = document.querySelector(".img-encrip");
+    let Mensaje = document.getElementById("mensaje-principal");
+    let parrafo = document.getElementById("parrafo-in");
+    let imagen = document.querySelector(".img-encrip");
 
     let textoCifrado = tex
-                        .replace (/e/gi, "enter")
-                        .replace(/i/gi,"imes")
-                        .replace(/a/gi,"ai")
-                        .replace(/o/gi,"ober")
-                        .replace(/u/gi, "ufat");
-                        
-        if (document.getElementById("texto").value.length !=0){
-            document.getElementById("texto").value = textoCifrado;
-            document.getElementById("mensaje-principal").textContent ="texto Encriptado con Exito";
-            document.getElementById("parrafo-in").textContent = "";
-            document.querySelector(".img-encrip").src = "/img/idea.jpg";
-        }else{
-            document.querySelector(".img-encrip").src = "/img/img-deco.png";
-            alert("Ingresa algun texto");
-        }
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
 
+    if (tex.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        Mensaje.textContent = "texto Encriptado con Exito";
+        parrafo.textContent = "";
+        imagen.src = "/img/idea.jpg";
+    } else {
+        imagen.src = "/img/img-deco.png";
+        Mensaje.textContent = "Ningun Mensaje Encontrado";
+        parrafo.textContent = "Ingresa el Texto que deseas desencriptar";
+        alert("Ingresa algun texto");
+    }
+
+}
+
+function desencriptar() {
+    let tex = document.getElementById("texto").value;
+    let Mensaje = document.getElementById("mensaje-principal");
+    let parrafo = document.getElementById("parrafo-in");
+    let imagen = document.querySelector(".img-encrip");
+
+    
+    let textoCifrado = tex
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+
+    if (tex.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        Mensaje.textContent = "texto Desencriptado con Exito";
+        parrafo.textContent = "";
+        imagen.src = "/img/fun.jpg";
+    } else {
+        imagen.src = "/img/img-deco.png";
+        Mensaje.textContent = "Ningun Mensaje Encontrado";
+        parrafo.textContent = "Ingresa el Texto que deseas desencriptar";
+        alert("Ingresa algun texto");
+    }
 }
