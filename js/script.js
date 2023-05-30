f01db31bd257880534d6ce102205c8e0ea029d1c
Adolf-Angel <adotolentinosan90@gmail.com>
2023-05-29 23:52:03 -0600

    se agrego la funcion encriptar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 
-const encriptar = document.querySelector(".btn-encriptar")
-encriptar.addEventListener('click', (e) =>{
-    const texto = document.querySelector("textin");
-   // e.preventdefault();
+function encriptar(){
+    let tex = document.getElementById("texto").value;
+    // let Mensaje = document.getElementById("mensaje-principal");
+    // let parrafo = document.getElementById("parrafo-in");
+    // let imagen = document.querySelector(".img-encrip");
 
-    console.log("encriptando");
+    let textoCifrado = tex
+                        .replace (/e/gi, "enter")
+                        .replace(/i/gi,"imes")
+                        .replace(/a/gi,"ai")
+                        .replace(/o/gi,"ober")
+                        .replace(/u/gi, "ufat");
+                        
+        if (document.getElementById("texto").value.length !=0){
+            document.getElementById("texto").value = textoCifrado;
+            document.getElementById("mensaje-principal").textContent ="texto Encriptado con Exito";
+            document.getElementById("parrafo-in").textContent = "";
+            document.querySelector(".img-encrip").src = "/img/idea.jpg";
+        }else{
+            document.querySelector(".img-encrip").src = "/img/img-deco.png";
+            alert("Ingresa algun texto");
+        }
 
-})
+}
